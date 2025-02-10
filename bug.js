8c3a4715a2572e18bfb0d627c5c8fb59bfd036d2
BugHunter-X <askzenai@gmail.com>
2025-02-10 03:08:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when you try to access a property of an object that is null or undefined.  This is common in React Native when dealing with asynchronous data fetching or navigation. For example, if you try to access user.name before the user object is fully loaded from an API call, you'll get this error.
